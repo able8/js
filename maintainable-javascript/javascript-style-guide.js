411abc2f0e28382f1b1b98fb6cef4b36fc50d6b5
able8 <able8@me.com>
2018-10-26 13:38:18 +0800

    add style guide

---
+++ maintainable-javascript/javascript-style-guide.js
@@ -0,0 +1,149 @@
+
+if (true) {
+    doSomething();
+}
+
+doSomething(argument1, argument2, argument3, argument4,
+        argument5);
+
+var name = "Nicholas";
+
+var count = 10;
+
+var price = 10.0;
+var price = 10.00;
+
+var num = oxA2;
+var num = 1e23;
+
+var person = null
+
+function getPerson() {
+    if (condition) {
+        return new person("Nicholas");
+    } else {
+        return null;
+    }
+}
+
+var person = getPerson();
+if (person !== null) {
+    doSomething();
+}
+
+if (typeof variable == "undefined") {
+    // do something
+}
+
+var found = (value[i] === item);
+
+if (found && (count > 10)) {
+ doSomething();
+}
+
+for (i = 0; i < count; i++) {
+    process(i);
+}
+
+var object = {
+
+    key1: value1,
+    key2: value2,
+
+    func: function () {
+        // do something
+    },
+
+    key3: value3
+};
+
+doSomething({
+    key1: value1,
+    key2: value2,
+});
+
+
+var count = 10,
+    name = "nicholas",
+    found = false,
+    empty;
+
+function doSomething(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+
+function outer() {
+
+    var count = 10,
+        name = "Nicholas",
+        found = false,
+        empty;
+
+    function inner() {
+        // code
+    }
+
+    // code that uses inner()
+}
+
+
+var value = (function() {
+    // function body
+
+    return {
+        message: "Hi"
+    }
+}());
+
+
+var accountNumber = "84"
+
+var flag = (i < count)
+
+var same = (a === b)
+
+var value = condition ? value1 : value2;
+
+count++;
+a = b
+
+return;
+return collection.size();
+return (sieze > 0 ? size : defaultSize);
+
+
+if (condition) {
+    statements
+}
+
+if (condition) {
+    statements
+} else {
+    statements
+}
+
+if (condition) {
+    statements
+} else if (condition) {
+    statements
+} else {
+    statements
+}
+
+switch (key) {
+    case value:
+
+        break;
+
+    default:
+        break;
+}
+
+
+
+try {
+
+} catch (error) {
+
+}
